6d793cded81bb10e662ca8798042071e6edf2fc0
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-05-04 01:08:30 +0600

    Auction Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './Navbar/Navbar'
-import Hero from './Hero/hero'
+import Hero from './Hero/Hero'
 import Cards from './Cards/Cards'
 import Footer from './Footer/Footer'
 
